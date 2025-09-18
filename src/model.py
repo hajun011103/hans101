6c71456b2085e28612b29e5d62efa5f4ab4fdf5a
Hajun Jang <138289826+hajun011103@users.noreply.github.com>
2025-09-18 13:54:15 +0900

    Update model.py

--- src/model.py
+++ src/model.py
@@ -14,14 +14,13 @@ class CNN(nn.Module):
         self.conv3 = nn.Conv2d(16, 32, 3, 1, 1)
         self.conv4 = nn.Conv2d(32, 64, 3, 1, 1)
         self.conv5 = nn.Conv2d(64, 128, 3, 1, 1)
-        self.conv6 = nn.Conv2d(128, 256, 3, 1, 1)
         self.pool = nn.MaxPool2d((2, 2))
         self.bn1 = nn.BatchNorm2d(6)
         self.bn2 = nn.BatchNorm2d(16)
         self.bn3 = nn.BatchNorm2d(32)
         self.bn4 = nn.BatchNorm2d(64)
         self.bn5 = nn.BatchNorm2d(128)
-        self.fc1 = nn.Linear(32 * 28 * 28, 120) # input_channels, output_channels
+        self.fc1 = nn.Linear(32 * 28 * 28, 120) # in_features, out_featuers
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 4) # 4 cat classes
         self.dropout = nn.Dropout(0.1)
@@ -37,9 +36,7 @@ class CNN(nn.Module):
         # x = self.pool(F.relu(self.conv3(x)))
         # x = self.pool(F.relu(self.conv4(x)))
         # x = self.pool(F.relu(self.conv5(x)))
-        # x = self.pool(F.relu(self.conv6(x)))
         x = torch.flatten(x, 1)
-        # x = F.relu(self.fc1(x))
         x = self.dropout(F.relu(self.fc1(x)))
         x = F.relu(self.fc2(x))
         output = self.fc3(x)
